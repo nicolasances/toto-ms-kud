d937a788917832b9a17cc2007c4a726d75667696
nicolasances <nicolas.matteazzi@gmail.com>
2024-10-04 09:52:40 +0200

    Updated separators for 2024

--- kud/util/kudutil.py
+++ kud/util/kudutil.py
@@ -14,5 +14,8 @@ def get_separators(year, month):
     elif int(year) == 2020 and int(month) >= 9: 
         dec_separator = ','
         thousands_separator = '.'
+    elif int(year) == 2024 and int(month) >= 6:
+        dec_separator = '.'
+        thousands_separator = ','
 
     return dec_separator, thousands_separator
